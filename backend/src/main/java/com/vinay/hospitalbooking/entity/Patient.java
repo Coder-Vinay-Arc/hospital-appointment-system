2318dc20a99a3d34f671d60c6c5cb928e62e6434
Vinay Gupta <vinayjee04@gmail.com>
2026-08-06 16:50:25 +0530

    New Update

--- backend/src/main/java/com/vinay/hospitalbooking/entity/Patient.java
+++ backend/src/main/java/com/vinay/hospitalbooking/entity/Patient.java
@@ -1,7 +1,6 @@
 package com.vinay.hospitalbooking.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
